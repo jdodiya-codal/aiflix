a7b42c9327a32e37b8201bd5f15cb69b969ff0b0
jdodiya-codal <jdodiya@codal.com>
2025-06-18 13:56:10 +0530

    removed openAI

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import MovieListAPIView, AskAIView, AskHuggingFaceAIView, MovieReviewsAPIView, MovieSummaryAPIView
+from .views import MovieListAPIView, AskHuggingFaceAIView, MovieReviewsAPIView, MovieSummaryAPIView
 
 urlpatterns = [
     path('movies/', MovieListAPIView.as_view(), name='movie-list'),
-    path('ask-ai/', AskAIView.as_view()),
     path('ask-hugging-face-ai/', AskHuggingFaceAIView.as_view()),
     path('movie-summary/', MovieSummaryAPIView.as_view(), name='movie-summary'),
     path('movie-reviews/', MovieReviewsAPIView.as_view(), name='movie-reviews'),
